adabf117c6cc5d3ef1612bd04857b0e979db3d00
alkeshnagar56 <alkeshnagar56@gmail.com>
2024-10-05 00:28:21 +0530

    fifth assignment

--- src/Component/Cards.jsx
+++ src/Component/Cards.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-// import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const Cards = () => {
     const navigate = useNavigate()
   return (
     <>
     <div>Welcome to the Cards...</div>
-    {/* <button onClick={()=>navigate(-1)}>Go Back</button> */}
+    <button onClick={()=>navigate(-1)}>Go Back</button>
     </>
   )
 }
